53676dc20c4feb5e6ea36636122c3c0f563e473c
terentyevkirill <kirillterentyev1999@gmail.com>
2020-03-24 15:50:58 +0100

    Flags don't show if there's no iso code provided by API

--- app/src/main/java/com/terentiev/coronatracker/data/CountryInfo.kt
+++ app/src/main/java/com/terentiev/coronatracker/data/CountryInfo.kt
@@ -1,25 +1,10 @@
 package com.terentiev.coronatracker.data
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
 data class CountryInfo(
-    @SerializedName("iso2")
-    @Expose
     val iso2: String,
-    @SerializedName("iso3")
-    @Expose
     val iso3: String,
-    @SerializedName("_id")
-    @Expose
     val _id: String,
-    @SerializedName("lat")
-    @Expose
     val lat: Double,
-    @SerializedName("long")
-    @Expose
     val long: Double,
-    @SerializedName("flag")
-    @Expose
     val flag: String
 )
--- app/src/main/java/com/terentiev/coronatracker/ui/dashboard/CountriesAdapter.kt
+++ app/src/main/java/com/terentiev/coronatracker/ui/dashboard/CountriesAdapter.kt
@@ -1,5 +1,6 @@
 package com.terentiev.coronatracker.ui.dashboard
 
+import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,6 +28,14 @@ class CountriesAdapter(countriesEvents: ItemEvents) :
             itemView.tv_today_cases_num.text = country.todayCases.toString()
             itemView.tv_today_deaths_num.text = country.todayDeaths.toString()
             itemView.tv_recovered_num.text = country.recovered.toString()
+            if (country.countryInfo.iso2 == "NO DATA") {
+                itemView.iv_flag.visibility = View.GONE
+            } else {
+                Glide
+                    .with(itemView.context)
+                    .load("https://www.countryflags.io/${country.countryInfo.iso2}/shiny/64.png")
+                    .into(itemView.iv_flag)
+            }
             itemView.setOnLongClickListener {
                 listener.onItemLongClicked(country)
                 true
@@ -34,10 +43,7 @@ class CountriesAdapter(countriesEvents: ItemEvents) :
             itemView.setOnClickListener {
                 listener.onItemClicked(position, country)
             }
-            Glide
-                .with(itemView.context)
-                .load(country.countryInfo.flag)
-                .into(itemView.iv_flag)
+
         }
     }
 
